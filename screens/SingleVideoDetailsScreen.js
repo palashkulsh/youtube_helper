35524ff1256e94a5f0ddc2fcc4cc9d7538f56d27
palash <palash.kulsh@ymail.com>
2024-04-20 10:42:47 +0530

    working with zoom and full screen

--- screens/SingleVideoDetailsScreen.js
+++ screens/SingleVideoDetailsScreen.js
@@ -40,14 +40,16 @@ const SingleVideoDetailsScreen = ({ route }) => {
         <View style={styles.container}>
             <View style={[styles.videoContainer, isFullScreen && styles.fullScreenContainer]}>
                <YoutubePlayer
-                    height={isFullScreen ? width*0.95 : 200}
-                    width={isFullScreen ? height*0.95 : '100%'}
+                    height={isFullScreen ? width : 200}
+                    width={isFullScreen ? height : '100%'}
                     videoId={videoId}
                     play={true}
                     onChangeState={(event) => console.log(event)}
                     onFullScreenChange={handleFullScreenChange}
-                   contentScale={contentScale}
 		   allowWebViewZoom={true}
+		   initialPlayerParams={{
+		       preventFullScreen: true		       
+		   }}
 		   webViewProps={{
                         userAgent: 'Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.71 Safari/537.36',
                    }}
@@ -84,12 +86,12 @@ const styles = StyleSheet.create({
         aspectRatio: 16 / 9,
     },
     fullScreenContainer: {
-        width: height,
-        height: width,
-        transform: [{ rotate: '90deg' }, { translateX: (height - width) / 2 }, { translateY: (height - width) / 2 }],
+        width: height*1,
+        height: width*1,
+        transform: [{ rotate: '90deg' }, { translateX: (height - width) / 2.5  }, { translateY: (height - width) / 2.5 }],
     },
     videoDetails: {
-        padding: 16,
+
     },
 });
 
